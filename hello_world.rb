d1e08a264f2c4b9f881741e7cda7927a6510c9a0
randheer <randheer.dakshana15@gmail.com>
2024-07-01 20:09:47 +0530

    learning ruby

--- hello_world.rb
+++ hello_world.rb
@@ -1,4 +1,2 @@
 puts "hello world" # put puts newline at the end
-print "hello world" # print doens't puts new line at the end
-p "hello world"
-a = "hello world"
+print "hello world" #print doens't puts new line at the end
