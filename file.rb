73cf250bd779ecf591e2b3358859376e8f57f8a0
randheer <randheer.dakshana15@gmail.com>
2024-07-02 11:38:38 +0530

    configured ruby coding in vscode

--- file.rb
+++ file.rb
@@ -1,14 +1,16 @@
-f = File.open("new.txt", "w+")
-f.puts "first line"
-f.write "seocnd line \n"
-f.write "thire line"
+# frozen_string_literal: true
+
+f = File.open('new.txt', 'w+')
+f.puts 'first line'
+f.write 'seocnd line \n'
+f.write 'thire line'
 f.close
 
 # File.open("new.txt", "r") do |file|
 #   puts file.read
 # end
 
-f = File.open("new.txt", "r")
+f = File.open('new.txt', 'r')
 puts f.readlines
 
 f.close
